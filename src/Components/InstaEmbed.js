fdddef4fb6f7a565eb7d422a5eaf76a26cd034c3
Strong10mede <kmayur809@gmail.com>
2022-08-26 13:45:46 +0530

    instaembed.js working

---
+++ src/Components/InstaEmbed.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import CopyrightIcon from "@mui/icons-material/Copyright";
+import { Avatar, Button } from "@mui/material";
+function InstaEmbed() {
+  return (
+    <div className="instaEmbed">
+      <img
+        className="instaEmbed__image"
+        src="https://1000logos.net/wp-content/uploads/2017/02/Logo-Instagram.png"
+        alt=""
+      />
+      <div className="instaEmbed__text">
+              <h4>Hello peeps 🙋</h4>
+              <h4>Welcome to Instagram Clone App ⚡ !!!</h4>
+
+        <p>
+          Instagram is an American photo and video sharing social networking service owned by Facebook. This project is a Clone of the original Instagram UI. This Project provides some basic features, similar to the original application.
+          <br />
+          <br />
+          <b>P.S.</b> This project has been developed for learning purposes, and it has nothing to do with the original Application.
+          <br />
+          <br />
+          <b>Features : </b>
+          <ul>
+            <li>User Authentication : Sign In and Sign Up</li>
+            <li>Image Uploading for creating new Posts</li>
+            <li>User can add Comments to the Posts</li>
+            <li>An Awesome User-Interface</li>
+          </ul>
+          <br />
+        </p>
+        <h4>Hope you have a great time, exploring the application in and out ✌ !!!</h4>
+      </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default InstaEmbed
